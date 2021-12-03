d2c4e9571d835e9556065c33766775e6510e667a
Karl R. Wurst <karl@w-sts.com>
2021-12-03 20:04:09 +0000

    feat: add orders display

--- bin/backend-data.sh
+++ bin/backend-data.sh
@@ -6,3 +6,7 @@ curl -d '{"name":"Cereal"}' -H "Content-Type: application/json" -X POST http://l
 curl -d '{"name":"Milk"}' -H "Content-Type: application/json" -X POST http://localhost:10001/items
 curl -d '{"name":"Canned Tuna"}' -H "Content-Type: application/json" -X POST http://localhost:10001/items
 curl -d '{"name":"Soup"}' -H "Content-Type: application/json" -X POST http://localhost:10001/items
+
+curl -d '{"email":"xramirez@college.edu", "preferences":"Cherios", "restrictions":"Soy", "items":[{"_id":"61aa5a298153980008823b54", "name":"Cereal"}]}' -H "Content-Type: application/json" -X POST http://localhost:10001/orders
+curl -d '{"email":"tbecker@college.edu", "preferences":"Blueberries", "restrictions":"Peanuts", "items":[{"_id":"61aa5a298153980008823b54", "name":"Cereal"},{"_id":"61aa5a298153980008823b52", "name":"Milk"}, {"_id":"61aa73a98153980008823b71", "name":"Fruit"} ]}' -H "Content-Type: application/json" -X POST http://localhost:10001/orders
+curl -d '{"email":"pnguyen@college.edu", "preferences":"a", "restrictions":"a", "items":[{"_id":"61aa5a298153980008823d23", "name":"Shrimp"},{"_id":"61aa5a298153980008823ef1", "name":"Noodles"}]}' -H "Content-Type: application/json" -X POST http://localhost:10001/orders
--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,27 +1,96 @@
 <template>
   <div>
-    This will hold the page for managing orders.
+    <table>
+      <tr>
+        <td>_id</td><td>email</td>
+      </tr>
+      <tr v-for="order in orders" :key="order._id">
+        <td>{{order._id}}</td><td>{{order.email}}</td><td><button @click="viewOrder(order._id)">Order Details</button></td>
+      </tr>
+    </table>
+  </div>
+  <p></p>
+<div v-if="order !== null">
+    <table>
+      <tr>
+        <td>_id</td><td>{{order._id}}</td>
+      </tr>
+      <tr>
+        <td>email</td><td>{{order.email}}</td>
+      </tr>
+      <tr>
+        <td>preferences</td><td>{{order.preferences}}</td>
+      </tr>
+      <tr>
+        <td>restrictions</td><td>{{order.restrictions}}</td>
+      </tr>
+      <tr>
+      <td>Items:</td>
+      <td>
+      <tr v-for="item in order.items" :key="item._id">
+        <td>{{item._id}}</td><td>{{item.name}}</td>
+      </tr>
+      </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-//import axios from 'axios';
+import axios from 'axios';
 
 export default {
   name: 'Orders',
   data(){
     return {
-
+      orders: [],
+      order: null,
     }
   }, 
   props: {
 
   },
-  async mounted() {
-
+  async mounted () {
+    try {
+        this.getOrders();
+    }
+    catch (error) {
+        console.error(error);
+    }
+  },
+  watch: {
+    // call again the method if the route changes
+    '$route': 'getOrders'
   },
   methods: {
-    
+    async getOrders() {
+      try {
+        let ordersUrl = "http://localhost:10001/orders";
+        console.log(ordersUrl);
+        let response = await axios.get(ordersUrl);
+        console.log(response);
+        this.orders = response.data;
+        console.log(this.orders);
+        this.status = 'loaded';
+      } catch (error) {
+        console.error(error);
+        this.status = 'errored';  
+      }
+    },
+    async viewOrder(id) {
+      console.log(id);
+      try {
+        let ordersUrl = "http://localhost:10001/orders/" + id;
+        console.log(ordersUrl);
+        let response = await axios.get(ordersUrl);
+        this.order = response.data;
+        console.log(this.order);
+        this.status = 'loaded';
+      } catch (error) {
+        console.error(error);
+        this.status = 'errored';  
+      }
+    }
   }
 }
 </script>
